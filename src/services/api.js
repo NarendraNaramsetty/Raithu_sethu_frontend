11c52fa0ddbb7a3c354630366e58568e1287e9d0
Narendra Naramsetty <narinaramsetty@gmail.com>
2026-09-25 10:44:02 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -9,7 +9,7 @@
 
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL?.replace(/\/$/, '') ||
-  'http://127.0.0.1:8000/api';
+  'https://raithu-sethu-api.onrender.com/api';
 
 async function request(endpoint, options = {}) {
   const token = localStorage.getItem('raithu_auth_token');
